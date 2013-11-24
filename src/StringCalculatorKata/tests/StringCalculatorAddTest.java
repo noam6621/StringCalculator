ebee1ed26f1f92daa72ba766e9ef34a7d574430f
Noam Silver <noam6621@aim.com>
2013-11-24 11:47:44 +0200

    RED: Initial red

---
+++ src/StringCalculatorKata/tests/StringCalculatorAddTest.java
@@ -0,0 +1,14 @@
+package StringCalculatorKata.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorAddTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
